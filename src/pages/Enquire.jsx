2e69d1967a11c7596706c8b14e3e4ec9a9b51d76
Social Media Shop <socialmediashopza@gmail.com>
2026-05-20 11:39:47 +0200

    Add Facebook Pixel and Lead tracking

--- src/pages/Enquire.jsx
+++ src/pages/Enquire.jsx
@@ -153,6 +153,7 @@ export default function Enquire() {
       })
     })
     .then(() => {
+      if (window.fbq) window.fbq('track', 'Lead');
       setSubmitted(true);
       window.scrollTo({ top: 0, behavior: "smooth" });
     })
