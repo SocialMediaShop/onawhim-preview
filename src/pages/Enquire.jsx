020d80cae16fb80c339073dd1ab7feb8860862e0
Social Media Shop <socialmediashopza@gmail.com>
2026-05-27 11:52:25 +0200

    Install new Meta Pixel tracking code

--- src/pages/Enquire.jsx
+++ src/pages/Enquire.jsx
@@ -182,6 +182,7 @@ export default function Enquire() {
       })
     })
     .then(() => {
+      if (window.fbq) window.fbq('track', 'Lead');
       setSubmitted(true);
       window.scrollTo({ top: 0, behavior: "smooth" });
     })
